b19afe983e17fa5ef7736d0eccde9c6fa5edf93d
Angello Velarde <deangellovelarde@gmail.com>
2025-12-19 19:15:51 +0800

    Final Project 30 Days to Learn Laravel - Blade and Tailwind techniques (second half)

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,4 +1,4 @@
-<div class="p-4 bg-white/5 rounded-xl flex gap-x-6">
+<x-panel class="flex gap-x-6">
     <div>
         <x-employer-logo />
     </div>
@@ -6,7 +6,7 @@
     <div class=" flex-1 flex flex-col">
         <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
 
-            <h3 class="font-bold text-xl mt-3">Video Producer</h3>
+            <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">Video Producer</h3>
             <p class="text-sm text-gray-400 mt-auto">Full-Time From $60,000</p>
     </div>
 
@@ -19,4 +19,4 @@
         </div>
     </div>
 
-</div>
+</x-panel>
